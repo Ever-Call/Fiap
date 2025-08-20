d80b5e60cf02b5ed133cdaaa3a3fc583cfb71112
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 10:27:01 -0300

    Aprendemos utilizar componentes com React

---
+++ 5/blogpessoal/src/App.tsx
@@ -0,0 +1,10 @@
+//apagamos todo o conteúdo anteriormente aqui
+import Home from "./pages/home/Home";
+
+function App(){
+  return(
+    <Home/> // a gente usa nosso componete Home colocando aqui como uma tag de html
+  )
+}
+
+export default App
